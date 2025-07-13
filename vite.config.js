c7bd0ae2afce003e2725dbe4c4f42455f1cc1329
Shehanaaz-dotcom <157867096+Shehanaaz-dotcom@users.noreply.github.com>
2025-07-13 20:44:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Sz-porto/",
+  base: "/Sz-Porto/",
   css:{
     modules:{
       localsConvention:"camelCase"
