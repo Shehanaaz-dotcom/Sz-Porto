d71d0532657aff7616c1107e94e9ea72e2511954
Shehanaaz-dotcom <157867096+Shehanaaz-dotcom@users.noreply.github.com>
2025-07-14 12:18:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.jsx'
+ import { BrowserRouter , Routes, Route } from "react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Sz-Porto/">
+    <Routes>
+    <Route path ="*" element= {  <App />}/>
+      </Routes>
+    </BrowserRouter>
+  
   </StrictMode>,
 )
